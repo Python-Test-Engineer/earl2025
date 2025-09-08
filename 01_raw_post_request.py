7e68684a6dc0e1a0d3d6993c94de573613332c8b
Craig West <iwswordpress@gmail.com>
2025-09-08 14:38:13 +0100

    pptx

--- 01_raw_post_request.py
+++ 01_raw_post_request.py
@@ -7,34 +7,32 @@ from rich.console import Console
 
 console = Console()
 load_dotenv(find_dotenv(), override=True)  # read local .env file
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # get API key from .env file
-system_prompt = ""
+# get API key from .env file
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+system_prompt = "You are a helpgul assistant."
 user_prompt = "Tell me about Brighton in 100 words or so."
-temperature = 0
 
 
-model = "gpt-4o-mini"  # Model
-model_endpoint = "https://api.openai.com/v1/chat/completions"  # just one endpoint
+MODEL = "gpt-4o-mini"
+# just one endpoint and does not change based on request
+model_endpoint = "https://api.openai.com/v1/chat/completions"
 headers = {
     "Content-Type": "application/json",  # Authorisation
     "Authorization": f"Bearer {OPENAI_API_KEY}",
 }
 # payload structure may vary from LLM Organisation but it is a text string.
 payload = {
-    "model": model,
+    "model": MODEL,
     "messages": [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": user_prompt},
     ],
-    # additional parameters
-    "stream": False,
-    "temperature": temperature,
 }
-# Use HTTP POST method
+# Use HTTP POST method - a web form in essence
 response = requests.post(
-    url=model_endpoint,  # The endpoint we are sending the request to. Low Temperature:
+    url=model_endpoint,  # The endpoint we are sending the request to.
     headers=headers,  # Headers for authentication etc
     data=json.dumps(payload),  # Inputs, Context, Instructions, Additonal parameters
 ).json()
 
-console.print(response)  # print the response
+console.print(response)
