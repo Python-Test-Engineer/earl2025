4c7d309f60a151b39054fbb0239cd9fc2ea0b518
Craig West <iwswordpress@gmail.com>
2025-09-08 09:46:14 +0100

    add demos

--- 01_raw_post_request.py
+++ 01_raw_post_request.py
@@ -1,12 +1,15 @@
 import os
 import json
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
 import requests
+from rich.console import Console
 
-api_key = ""
+console = Console()
+load_dotenv(find_dotenv(), override=True)  # read local .env file
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # get API key from .env file
 system_prompt = ""
-user_prompt = ""
+user_prompt = "Tell me about Brighton in 100 words or so."
 temperature = 0
 
 
@@ -14,7 +17,7 @@ model = "gpt-4o-mini"  # Model
 model_endpoint = "https://api.openai.com/v1/chat/completions"  # just one endpoint
 headers = {
     "Content-Type": "application/json",  # Authorisation
-    "Authorization": f"Bearer {api_key}",
+    "Authorization": f"Bearer {OPENAI_API_KEY}",
 }
 # payload structure may vary from LLM Organisation but it is a text string.
 payload = {
@@ -33,3 +36,5 @@ response = requests.post(
     headers=headers,  # Headers for authentication etc
     data=json.dumps(payload),  # Inputs, Context, Instructions, Additonal parameters
 ).json()
+
+console.print(response)  # print the response
